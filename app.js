b9ceb6e6fe88c359d55fa4f413fa1e68be02796f
Ceren Çiçek <cicekceren46@gmail.com>
2026-06-21 01:16:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require('dotenv').config({ path: __dirname + '/.env' });
-//1.az önce indirdiğin MySQl eklentisini koda dahil edicez
+//az önce indirdiğim MySQl eklentisini koda dahil edicez
 const mysql = require ('mysql2');
-//2.MySQL veri tabanımıza bağlanmak için gerekli bilgileri gireceğiz
+//MySQL veri tabanımıza bağlanmak için gerekli bilgileri gireceğiz
 const bağlanti = mysql.createConnection ({
     host: process.env.HC_HOST, // bilgisayardda çalıştığı için localhost
     user: process.env.HC_USER, // XAMPP veya workbencj varsayılan kullanıcı adı
-password: process.env.HC_PASSWORD, // eğer şifren yoksa arayı boş bırak
+password: process.env.HC_PASSWORD, //şifre kısmı
 database: process.env.HC_NAME // ilk adımda oluşturduğum veritabanımın ismii
 });
-//3.Bağlantıyı resmi olarak başlatıyoruz.
+//Bağlantıyı resmi olarak başlatıyoruz.
 bağlanti.connect((hata)=> {
     if (hata) {
         console.error ('Veri tabanına bağlanırken hata oluştu:' + hata.stack);
@@ -17,7 +17,7 @@ bağlanti.connect((hata)=> {
     console.log ('Başarılı ! MySQL Veri Tabanına Bağlanıldı.') ;
 });
  
-//4. Veri tabanına kitapları getirecek SQL sorgumuzu çalıştırıyoruz.
+// Veri tabanına kitapları getirecek SQL sorgumuzu çalıştırıyoruz.
 bağlanti.query('SELECT * FROM Kitaplar ', (hata, sonuclar) => {
     if (hata) throw hata;
     console.log ('\n --- KÜTÜPHANEDEKİ KİTAPLARIMIZ---');
